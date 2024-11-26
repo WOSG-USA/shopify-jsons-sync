8fb6ca494a6bc14cadb85d12eb5e35be2685aaed
Spencer Karges <spencer@karges.org>
2024-11-26 10:18:31 -0500

    add templates/*.json to theme pull

--- dist/index.js
+++ dist/index.js
@@ -46,7 +46,7 @@ async function run() {
             process.chdir(workingDirectory);
         }
         await (0, utils_1.cleanRemoteFiles)();
-        await (0, exec_1.exec)(`shopify theme pull --only config/*_data.json --only templates/**/*.json --only locales/*.json --live --path remote --store ${store} --verbose`, [], utils_1.EXEC_OPTIONS);
+        await (0, exec_1.exec)(`shopify theme pull --only config/*_data.json --only templates/**/*.json --only templates/*.json --only locales/*.json --live --path remote --store ${store} --verbose`, [], utils_1.EXEC_OPTIONS);
         const localeFilesToPush = await (0, utils_1.syncLocaleAndSettingsJSON)();
         const newTemplatesToPush = await (0, utils_1.getNewTemplatesToRemote)();
         await (0, utils_1.sendFilesWithPathToShopify)([...localeFilesToPush, ...newTemplatesToPush], {
